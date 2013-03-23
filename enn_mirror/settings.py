7e7d37b52f087b1d3a3c22e49b5afca513e8e94c
aldarund <rhbcnfkk1>
2013-03-23 11:32:27 +0300

    local settings

--- enn_mirror/settings.py
+++ enn_mirror/settings.py
@@ -9,16 +9,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/orion/projects/enn_mirror/tmpDB',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -102,12 +93,7 @@ ROOT_URLCONF = 'enn_mirror.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'enn_mirror.wsgi.application'
 
-TEMPLATE_DIRS = (
-    "/home/orion/projects/enn_mirror/enn_mirror/templates"
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -151,3 +137,7 @@ LOGGING = {
         },
     }
 }
+try:
+    import local_settings
+except:
+    pass
