16ee79f21421067532f05473e14633237bf65b13
aldarund <rhbcnfkk1>
2013-10-10 00:29:31 +0300

    auto refresh test

---
+++ enn_mirror/base/page_replace.py
@@ -0,0 +1,38 @@
+from django.contrib.sites.models import Site
+
+__author__ = 'orion'
+
+current_site = Site.objects.get_current()
+def get_auto_refresh_code(mirror_code, current_level):
+    auto_refresh_url = "http://%s/auto_up/%s/%s/" % (current_site.domain, mirror_code, current_level)
+
+    auto_refresh_code = """
+    <script type="text/javascript">
+   //     {#        var st = $("a",".levelstats").attr("href");#}
+     //   {#        var c_level = st.substr(st.search("level=[0-9]+")+6,st.indexOf("&")-st.search("level=[0-9]+")-6);#}
+        var c_level = "%s";
+        var URL = "%s";
+        function autorefresh() {
+            $.ajax({
+                url: URL,
+                success: function (data) {
+                    if (data != c_level) {
+                        document.location.href = document.location.href;
+
+                    } else {
+                        autorefresh();
+                    }
+                },
+                error: function () {
+                    setTimeout(function(){autorefresh();},2000);
+                }
+            })
+        }
+        $(document).ready(function () {
+            autorefresh();
+        });
+
+    </script>
+
+"""%(current_level,auto_refresh_url)
+    return auto_refresh_code
