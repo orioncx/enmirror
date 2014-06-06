56860e6b48126f14928ca3ceeab52256f893c8eb
orion <orion.cx@gmail.com>
2014-06-06 21:45:13 +0300

    fail fix

--- enn_mirror/base/urls.py
+++ enn_mirror/base/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^auto_play/(?P<code>[a-z0-9_]{3,10})/$', fast_view_game,name="fast_view_game"),
     url(r'^auto_up/(?P<code>[a-z0-9_]{3,10})/(?P<level_id>[a-z0-9_]{1,3})/$', auto_up,name="auto_up"),
     url(r'^mirror_data/$', dyn_mirror, name="mirror_data"),
-    url(r'^mirror/$', mirror, name="mirror"),
+    url(r'^mirror_2389759/$', mirror, name="mirror"),
     url(r'^gen_new_key/$', gen_new_key, name="gen_new_key"),
     url(r'^img/(?P<code>[0-9])/xxxZXcd123123.jpeg$', get_antimirror_img, name="get_antimirror_img"),
 )
